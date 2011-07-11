9da20ec2eada2727aedda9c66359625f60742396
Bernard Lambeau <blambeau@gmail.com>
2011-07-11 17:46:04 +0200

    Extended the example with inline and non-inline

--- examples/create.rb
+++ examples/create.rb
@@ -3,5 +3,8 @@ require "sqlize"
 
 schema  = YAML.load File.read(File.expand_path("../schema-v1.0.0.yaml", __FILE__))
 defs = SQLize::Definitions.postgres
+
 puts defs.instantiate("/ddl/schema/create-inline", schema)
+puts "-"*80
+puts defs.instantiate("/ddl/schema/create", schema)
 
