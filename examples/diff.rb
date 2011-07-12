0a2214d666e04cf44c358b65194baaef175d972d
Bernard Lambeau <blambeau@gmail.com>
2011-07-12 17:46:19 +0200

    Added an empty schema to examples

--- examples/diff.rb
+++ examples/diff.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path("../../lib",__FILE__) 
 require "sqlize"
 
-s1  = YAML.load File.read(File.expand_path("../schema-v1.0.0.yaml", __FILE__))
+s1  = YAML.load File.read(File.expand_path("../schema-v0.0.1.yaml", __FILE__))
 s2  = YAML.load File.read(File.expand_path("../schema-v1.0.1.yaml", __FILE__))
 s1 = [s1]
 s2 = [s2]
